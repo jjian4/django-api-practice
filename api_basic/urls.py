3f34769d7c401514c44038e852a4c601e108a29a
jjian4 <jjian@umich.edu>
2020-05-24 16:22:05 -0400

    Authentication, view sets, generic view sets

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,29 @@
-from django.urls import path
+from django.urls import path, include
 # from .views import article_list, article_detail
-from .views import ArticleAPIView, ArticleDetailAPIView, GenericArticleAPIView, GenericArticleDetailAPIView
+from .views import ArticleAPIView, ArticleDetailAPIView, GenericArticleAPIView, GenericArticleDetailAPIView, ArticleViewSet, GenericArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+
+# Router for viewset
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+
+# Router for generic viewset
+router2 = DefaultRouter()
+router2.register('article', GenericArticleViewSet, basename='article')
+
 
 urlpatterns = [
+
+    # GENERIC VIEWSETS
+    path('genviewset/', include(router2.urls)),
+
+    # VIEWSETS: handles both articles and article detail
+    path('viewset/', include(router.urls)),
+
+    # GENERIC VIEWS
+    path('generic/article/', GenericArticleAPIView.as_view()),
+    path('generic/article/<int:id>', GenericArticleDetailAPIView.as_view()),
 
     # CLASS BASED VIEWS
     path('article/', ArticleAPIView.as_view()),
@@ -11,10 +32,5 @@ urlpatterns = [
     # FUNCTION BASED VIEWS (look at views.py)
     # path('article/', article_list),
     # path('article/<int:pk>/', article_detail),
-
-    
-    # GENERIC VIEWS
-    path('generic/article/', GenericArticleAPIView.as_view()),
-    path('generic/article/<int:id>', GenericArticleDetailAPIView.as_view())
 
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,18 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status, generics, mixins
+from rest_framework import status, generics, mixins, viewsets
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+
+# GENERIC VIEW SETS
+class GenericArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+
+# -------------------------------------------------
+
+# VIEW SETS: Simpler than views because no need to create separate classes for list vs. detail
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# -------------------------------------------------
 
 # GENERIC (pre-built) VIEWS
 
 class GenericArticleAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+
+    # 3 auth options: Sessions, Basic, and Token
+    # authentication_classes = [SessionAuthentication, BasicAuthentication] # Check if theres session, if not then use basic
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id=None):
         return self.list(request)
